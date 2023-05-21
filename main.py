f0e09f324cf0e070126eca5eac8ffcb7395bf6ef
jamesrhunt <jamesrhunt@gmail.com>
2023-05-21 12:57:24 +0200

    Performed further bivariate plot analysis and restructured main.py into sections.

--- main.py
+++ main.py
@@ -4,82 +4,54 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from src.dataExploration import *
 
+# Boolean switches
+doProfReports = False # generate profile reports
+doUnivariatePlots = False # make and save all univariate plots of features for yes/no attrition
+doBivariateCategorialPlots = False # do specific bivariate plots with 2xCategorical features
+doBivariateNumCatPlots = False # plot and save bivariate plots which are a combination of categorical and numeric features
 
 # Load the CSV file
 df = pd.read_csv('data/Crayon_case_employee-attrition.csv')
 
-# Split into Attrition or not
-#dfAttrition = df[df['Attrition'] == 'Yes']
-#dfNoAttrition = df[df['Attrition'] == 'No']
+if doProfReports:
+    # Split into Attrition or not
+    dfAttrition = df[df['Attrition'] == 'Yes']
+    dfNoAttrition = df[df['Attrition'] == 'No']
+    createProfilingReports(df, dfAttrition, dfNoAttrition)
 
-#createProfilingReports(df, dfAttrition, dfNoAttrition)
 
-
-# Dropping unecessary features (constant values):
+# Dropping unecessary features after looking at reports (constant values):
 df = df.drop(["EmployeeCount", "Over18", "StandardHours"], axis=1)
 
+########################################################################
 
+# DATA EXPORATION - CATEGORICAL AND NUMERIC SPLIT
+
+# Get categorical and numeric feature names, also checks data types
 categoricalFeatureNames, numericalFeatureNames = categoricAndNumeric(df)
+
 # Have a look at the numeric data ranges etc
 for column in df[numericalFeatureNames].columns:
     dfDescribe = df[column].describe()
     #print(dfDescribe)
     print("Min: {},\t Max: {},\t Range: {}, {}".format(dfDescribe['min'],dfDescribe['max'],dfDescribe['max']-dfDescribe['min'],column))
 
+########################################################################
 
-# UNIVARIATE PLOTS
+# DATA EXPORATION - PLOTTING
 
-# CATEGORICAL
+# Plot and save all of the possible univariate plots
+if doUnivariatePlots:
+    univariatePlots(df, categoricalFeatureNames, numericalFeatureNames)
 
-if 0:
-    for column in categoricalFeatureNames:
-        plt.figure()
-        sns.histplot(x = df[column], hue=df['Attrition'], multiple="dodge", 
-                    stat = 'percent', shrink = 0.8, common_norm=False)
-        plt.savefig("data/plots/dataExploration/categorical/"+column+"-AttritionBar.png")
-        #plt.show()
+# Do some bivariate plots:
+if doBivariateCategorialPlots:
+    bivariateCatPlots(df)
+if doBivariateNumCatPlots:
+    bivariateNumCatPlots(df)
 
-# NUMERIC 
-
-if 0:
-    for column in numericalFeatureNames:
-        plt.figure()
-        sns.histplot(x = df[column], hue=df['Attrition'],  
-                        stat = 'percent', shrink = 1.0, common_norm=False)
-        plt.savefig("data/plots/dataExploration/numerical/"+column+"-AttritionHist.png")
-
-
-# BIVARIATE PLOTS
-
-# CATEGORICAL 
-
-if 0:
-    plt.figure()
-    sns.histplot(x = df['Department'], hue=df['EducationField'], multiple="dodge", 
-                    stat = 'percent', shrink = 0.8, common_norm=False)
-
-    plt.figure()
-    sns.histplot(x = df['EducationField'], hue=df['JobRole'], multiple="dodge", 
-                    stat = 'percent', shrink = 0.8, common_norm=False)
-
-    plt.figure()
-    sns.histplot(x = df['Department'], hue=df['JobRole'], multiple="dodge", 
-                    stat = 'percent', shrink = 0.8, common_norm=False)
+# For general testing of plot types etc:
+#miscPlots(df)
 
 
 
-
-# NUMERIC AND CATEGORICAL
-
-plt.figure()
-sns.catplot(data=df, x="OverTime", y="WorkLifeBalance", hue="Attrition", kind="box")
-
-plt.figure()
-sns.catplot(data=df, x="OverTime", y="MonthlyIncome", hue="Attrition", kind="box")
-
-# Show the plots
-plt.show()
-"""
-plt.figure()
-plt.scatter(df['MonthlyIncome'], df['MonthlyRate'])
-"""
--- src/dataExploration.py
+++ src/dataExploration.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 def createProfilingReports(df, dfAttrition, dfNoAttrition):
     from ydata_profiling import ProfileReport
     
@@ -14,7 +18,7 @@ def createProfilingReports(df, dfAttrition, dfNoAttrition):
 
 
 def categoricAndNumeric(df):
-    # 
+    
     # Get the categorical and numeric column names, 
     # and have a look at data types
     categoricalFeatureNames_ = []
@@ -27,10 +31,114 @@ def categoricAndNumeric(df):
         else:
             numericalFeatureNames_.append(columnName)
 
+    print("\n",len(categoricalFeatureNames_), "Categorical feature names:")
     print(categoricalFeatureNames_)
+    print("\n",len(numericalFeatureNames_), "Numerical feature names:")
     print(numericalFeatureNames_)
-    print(np.unique(dtypes))
+    print("\n",len(dtypes), "features in total.")
     # show all unique data types
-    print(len(categoricalFeatureNames_), len(numericalFeatureNames_), len(dtypes))
+    print("\nUnique data types:")
+    print(np.unique(dtypes))
+    
+    
     return categoricalFeatureNames_, numericalFeatureNames_
     
+
+
+def univariatePlots(df_, categoricalFeatureNames_, numericalFeatureNames_):
+
+    # UNIVARIATE PLOTS 
+
+    # CATEGORICAL
+
+    for column in categoricalFeatureNames_:
+        plt.figure()
+        sns.histplot(x = df_[column], hue=df_['Attrition'], multiple="dodge", 
+                    stat = 'percent', shrink = 0.8, common_norm=False)
+        plt.savefig("data/plots/dataExploration/categorical/"+column+"-AttritionBar.png")
+        #plt.show()
+
+    # NUMERIC 
+
+    for column in numericalFeatureNames_:
+        plt.figure()
+        sns.histplot(x = df_[column], hue=df_['Attrition'],  
+                        stat = 'percent', shrink = 1.0, common_norm=False)
+        plt.savefig("data/plots/dataExploration/numerical/"+column+"-AttritionHist.png")
+
+
+# BIVARIATE PLOTS - CATEGORICAL 
+def bivariatePlots_categorical(df_, catFeat1_, catFeat2_):
+    # This is just the function to make the plots
+    plt.figure()
+    sns.histplot(x = df_[catFeat1_], hue=df_[catFeat2_], multiple="dodge", 
+                    stat = 'percent', shrink = 0.8, common_norm=False)
+    plt.xticks(rotation=20, ha='right')
+    plt.savefig("data/plots/dataExploration/categorical/bivariate/bivarPlot_cat"+catFeat1_+"_"+catFeat2_+".png")
+
+
+
+# BIVARIATE PLOTS - NUMERIC AND CATEGORICAL
+def bivariatePlots_numerCateg(df_, catFeat1_, numFeat_, catFeat2_):
+    # This is just the function to make the plots
+    plt.figure()
+    sns.catplot(data=df_, x=catFeat1_, y=numFeat_, hue=catFeat2_, kind="box")
+    plt.savefig("data/plots/dataExploration/categorical/bivariate/bivarPlot_Numcat"\
+                +catFeat1_+"_"+numFeat_+"_"+catFeat2_+".png")
+    
+def bivariateCatPlots(df_):
+    # Plot and save specific categorical plots
+
+    # Make a list of categorical features to pair for plotting against each other
+    bivariateCategoricalNames = [
+        #['Department', 'EducationField'],
+        #['EducationField', 'JobRole'],
+        #['Department', 'JobRole'],
+        #['BusinessTravel', 'MaritalStatus'],
+        ['Attrition', 'JobRole']
+        ]
+
+    # plot the pairs of categorical features
+    for item in bivariateCategoricalNames:
+        bivariatePlots_categorical(df_, item[0], item[1])
+
+    plt.show()
+
+def bivariateNumCatPlots(df_):
+    # plot and save bivariate plots which are a combination of categorical and numeric features
+
+    bivariateNumCatNames = [
+        ['EducationField', 'Education', 'Attrition'],
+        ['OverTime', 'MonthlyIncome', 'Attrition'],
+        ['JobLevel', 'MonthlyIncome', 'Attrition']
+        ]
+    for item in bivariateNumCatNames:
+        bivariatePlots_numerCateg(df_, item[0],item[1],item[2])
+
+    plt.show()
+
+def miscPlots(df_):
+    
+    sns.histplot(
+        df_, x="Education", y="JobRole",
+        bins=30, discrete=(True, False)
+    )
+    plt.show()
+
+    #plt.figure()
+    #sns.catplot(data=df, x="OverTime", y="WorkLifeBalance", hue="Attrition", kind="box")
+
+    #plt.figure()
+    #sns.catplot(data=df, x="OverTime", y="MonthlyIncome", hue="Attrition", kind="box")
+
+    #plt.figure()
+    #plt.scatter(df['JobLevel'],df['MonthlyIncome'])
+    #plt.show()
+    #total working years, age
+
+# Show the plots
+#plt.show()
+"""
+plt.figure()
+plt.scatter(df['MonthlyIncome'], df['MonthlyRate'])
+"""
