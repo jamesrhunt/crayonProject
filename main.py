d6aca959539fe1413ba50f87d8da0a940d52ab9e
jamesrhunt <jamesrhunt@gmail.com>
2023-05-23 16:49:53 +0200

    Restructured ML section, creating new functions in classificationModels.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from src.dataExploration import *
 from src.dataManipulation import *
+from src.classificationModels import *
 from sklearn.preprocessing import StandardScaler
 
 
@@ -119,10 +119,12 @@ print(df.columns)
 df = dropFeatures(df)
 # update the cat and num feature names lists
 categoricalFeatureNames, numericalFeatureNames = categoricAndNumeric(df)
-print(df.columns)
+#print(df.columns)
 
 ########################################################################
 
+# CLASSIFICATION MODELS 
+# 
 # SPLIT INTO TARGET (y_df) AND FEATURES (x_df)
 
 # save features into x_df
@@ -130,21 +132,23 @@ x_df = df.drop("Attrition", axis=1)
 
 # Update categorical features list
 categoricalFeatureNames.remove('Attrition')
-print(categoricalFeatureNames)
+#print(categoricalFeatureNames)
 
 # save target into y_df
 y_df = df["Attrition"]
  
+
+# Redefine categorical and numerical feature names so they are based on
+# whether they are actually categorical and numerical rather than on their
+# initial data types. This is important for SMOTE and standard scaling.
+categoricalFeatureNames, numericalFeatureNames = redefineFeatures(categoricalFeatureNames, numericalFeatureNames)
+
+
 ########################################################################
 
 # BUILD PIPELINE FOR ENCODING, SCALING AND FEATURE SELECTION
 
-from sklearn.pipeline import Pipeline
-from imblearn.pipeline import Pipeline as ImbPipeline
-from sklearn.compose import ColumnTransformer
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-import pandas as pd
+
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
@@ -152,24 +156,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import roc_curve, roc_auc_score
-import matplotlib.pyplot as plt
-
-
-# Redefine categorical and numerical feature names so they are based on
-# whether they are actually categorical and numerical rather than on their
-# initial data types. this is important for SMOTE and standard sampling
-print(categoricalFeatureNames)
-print(numericalFeatureNames)
-print(len(categoricalFeatureNames), len(numericalFeatureNames))
-# List of categorical features that are in "numericalFeatureNames" just because
-# they were pre-encoded in the dataset
-moveToCategorical = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 
-                     'JobLevel', 'JobSatisfaction', 'RelationshipSatisfaction',
-                      'StockOptionLevel',  'WorkLifeBalance']
-for featureName in moveToCategorical:
-    numericalFeatureNames.remove(featureName)
-    categoricalFeatureNames.append(featureName)
-print(len(categoricalFeatureNames), len(numericalFeatureNames))
 
 if 0:
     # Test drops based on random forest feature importance
@@ -180,58 +166,13 @@ if 0:
         x_df = x_df.drop([featureName], axis=1)
         categoricalFeatureNames.remove(featureName)
 
-# Define the numerical pipeline
-numericalPipeline = Pipeline([
-    ('scaler', StandardScaler())  # Numerical feature scaling
-])
-
-# First, we define the preprocessing steps in the preprocessor object. 
-# It consists of a ColumnTransformer with two transformers: 
-# numericalPipeline for scaling numerical features and OneHotEncoder() 
-# for one-hot encoding categorical features.
-# one hot encoder: drop=first: "dummy variable trap" avoidance.
-preprocessor = ColumnTransformer([
-    ('numerical', numericalPipeline, numericalFeatureNames),  # Apply scaling to numerical features
-    ('categorical', OneHotEncoder(drop='first'), categoricalFeatureNames)  # One-hot encode categorical features 
-    #('categorical', OneHotEncoder(), categoricalFeatureNames)  # One-hot encode categorical features 
-])
+# build pipeline
+pipeline, preprocessor = buildPipeline(categoricalFeatureNames, numericalFeatureNames)
 
 
-# (use imb pipeline for SMOTE)
-# The preprocessor is then included as part of the pipeline 
-# along with other steps such as SMOTE oversampling and a 
-# placeholder for the classification model.
-pipeline = ImbPipeline([
-    ('preprocessing', preprocessor),  # Preprocessing with separate transformations
-    ('sampling', SMOTE()),  # Apply SMOTE for oversampling
-    ('classification', None)  # Placeholder for the classification model
-])
-
-
-
-
-# Take a look at x_df after just the transformation step:
-x_transformed = preprocessor.fit_transform(x_df)
-print("Transformed Data:")
-# Get the encoded feature names 
-# Why tranformers [1][1]:
-# first [1] to access second transformer in prepocessing (one hot encoding)
-# second [1] because the information is stored as [transformer name, actual transformer]
-encoded_feature_names = preprocessor.transformers_[1][1].get_feature_names_out()
-# Combine numerical and categorical feature names
-feature_names = numericalFeatureNames + encoded_feature_names.tolist()
-#print(feature_names)
-df_transformed =  pd.DataFrame(x_transformed, columns=feature_names)
-print(df_transformed)
-
-# having a quick look at the profiling report for the transformed data
+# Take a look at x_df after the transformation step:
 if 0:
-    from ydata_profiling import ProfileReport
-    profile = ProfileReport(df_transformed, title="df_transformed")
-    profile.to_file("data/reports/df_transformed.html")
-
-
-
+    check_x_transformed(x_df, preprocessor, numericalFeatureNames)
 
 
 
@@ -243,15 +184,15 @@ classifiers = [
     SVC(probability=True)
 ]
 
-roc_data = []
+########################################################################
 
+# FIT CLASSIFIERS AND OBTRAIN SCORING METRICS
 
-# Next, we iterate over the classifiers in the classifiers list and 
-# evaluate their performance using cross-validation. For each classifier,
-# we set the current classifier in the pipeline using 
+# Iterate over the classifiers in the classifiers list and 
+# evaluate their performance using cross-validation. For each 
+# classifier, set the current classifier in the pipeline using 
 # pipeline.set_params(classification=classifier).
-
-
+roc_data = []
 for classifier in classifiers:
     pipeline.set_params(classification=classifier)  # Set the current classifier
     scores = cross_val_score(pipeline, x_df, y_df, cv=5)  # Perform cross-validation
@@ -263,6 +204,9 @@ for classifier in classifiers:
     report = classification_report(y_df, y_pred)  # Generate classification report
     print(report)
     print("")
+    # note: in the code above, calling cross_val twice, could collapse those 
+    # calls into one and save all required outputs
+    # it also calls cross_val_predict again for the ROC curve
 
     # ROC curve
     # get the predicted probabilities for each data point
@@ -277,7 +221,7 @@ for classifier in classifiers:
     # obtain feature importance and coefficient analysis if supported by the classifier
     pipeline.fit(x_df, y_df)
     
-
+    # if we're at the randomforestclassifier, get the feature importances:
     if isinstance(classifier, RandomForestClassifier) and hasattr(classifier, 'feature_importances_'):
         print("Feature Importance:")
 
@@ -305,38 +249,13 @@ for classifier in classifiers:
     #    coefficients_df = coefficients_df.sort_values('Coefficient', ascending=False)
     #    print(coefficients_df)
     #    print("")
-plt.figure()
-# Plot the sorted DataFrame using a bar chart
-plt.figure(figsize=(10, 6))
-plt.bar(importance_df['Feature'], importance_df['Importance'])
-plt.xlabel('Feature')
-plt.ylabel('Importance')
-plt.title('Sorted Feature Importance')
-plt.xticks(rotation=20, ha='right')
 
-# Calculate the cumulative importance
-cumulative_importance = np.cumsum(importance_df['Importance'])
-
-# Plot the cumulative importance using a line plot
-plt.figure(figsize=(10, 6))
-plt.plot(importance_df['Feature'], cumulative_importance)
-plt.xlabel('Feature')
-plt.ylabel('Cumulative Importance')
-plt.title('Cumulative Feature Importance')
-plt.xticks(rotation=20, ha='right')
+# plot the importance_df from the random forest classifier
+if 0:
+    plotRandomForestImportance(importance_df)
 
 # Plot ROC Curve
-fig, ax = plt.subplots()
-ax.plot([0, 1], [0, 1], 'k--')  # Diagonal line for reference
-for fpr, tpr, _, model_name, auc in roc_data:
-    ax.plot(fpr, tpr, label=model_name+str(auc))
-ax.set_xlabel('False Positive Rate')
-ax.set_ylabel('True Positive Rate')
-ax.set_title('Receiver Operating Characteristic (ROC) Curve')
-ax.legend()
-plt.show()
+if 1:
+    plotROC(roc_data)
 
 quit()
-
-
-   
